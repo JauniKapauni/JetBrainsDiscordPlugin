7dc0ffcd3cc65dd8ba3b8b4a0e1d7e2a2929a8cb
JauniKapauni <riches-goods.54@icloud.com>
2026-08-30 18:45:58 +0200

    feat(startup): log ide and project name

--- src/main/kotlin/com/github/jaunikapauni/jetbrainsdiscordplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/jaunikapauni/jetbrainsdiscordplugin/startup/MyProjectActivity.kt
@@ -1,5 +1,6 @@
 package com.github.jaunikapauni.jetbrainsdiscordplugin.startup
 
+import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
@@ -7,6 +8,12 @@ import com.intellij.openapi.startup.ProjectActivity
 class MyProjectActivity : ProjectActivity {
 
     override suspend fun execute(project: Project) {
-        thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        //thisLogger().warn("Don't forget to remove all non-needed sample code files with their corresponding registration entries in `plugin.xml`.")
+        val ideName = ApplicationInfo.getInstance().versionName
+        val projectName = project.name
+
+        println("IDE: $ideName")
+        println("Project: $projectName")
+
     }
 }
